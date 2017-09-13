db41f8624cd35e12d931adef08b63eb606f47ffb
roxnlopez <roxnlopez@gmail.com>
2017-09-13 15:16:54 -0600

    added authentication

---
+++ Y/app.js
@@ -0,0 +1,52 @@
+//   var database = firebase.database();
+
+//   //basic write operation
+//   function writeUserData(userId, name, email, imageUrl) {
+//   firebase.database().ref('users/' + userId).set({
+//     username: name,
+//     email: email,
+//     profile_picture : imageUrl
+//   });
+// }
+
+// //listen for value event
+// var starCountRef = firebase.database().ref('posts/' + postId + '/starCount');
+// starCountRef.on('value', function(snapshot) {
+//   updateStarCount(postElement, snapshot.val());
+// });
+
+// //read once...not expected to change
+// var userId = firebase.auth().currentUser.uid;
+// return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
+//   var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+//   // ...
+// });
+
+// //create a post
+// function writeNewPost(uid, username, picture, title, body) {
+//   // A post entry.
+//   var postData = {
+//     author: username,
+//     uid: uid,
+//     body: body,
+//     title: title,
+//     starCount: 0,
+//     authorPic: picture
+//   };
+
+//   // Get a key for a new Post.
+//   var newPostKey = firebase.database().ref().child('posts').push().key;
+
+//   // Write the new post's data simultaneously in the posts list and the user's post list.
+//   var updates = {};
+//   updates['/posts/' + newPostKey] = postData;
+//   updates['/user-posts/' + uid + '/' + newPostKey] = postData;
+
+//   return firebase.database().ref().update(updates);
+// }
+
+// // Create a new post reference with an auto-generated id
+// var newPostRef = postListRef.push();
+// newPostRef.set({
+//     // ...
+// });
