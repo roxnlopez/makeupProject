71187138d986ffb7f28d38c1909014a7b28d4e9a
roxnlopez <roxnlopez@gmail.com>
2017-09-14 16:55:46 -0600

    fixed edit and delete...now can edit and delete

--- Y/app.js
+++ Y/app.js
@@ -58,7 +58,7 @@ function del(key, muName) {
 }
 
 function buildEndPoint (key) {
-	return new Firebase('https://what-the-blush.firebaseio.com/makeup' + key);
+	return new Firebase('https://what-the-blush.firebaseio.com/makeup/' + key);
 }
 
 //this will get fired on initial load as well as when there is a change in the data
