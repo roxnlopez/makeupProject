c700eb6b63fad4add72d85ce91f0dd94cc3f7193
roxnlopez <roxnlopez@gmail.com>
2017-09-19 10:30:02 -0600

    added api into appJS

--- Y/api.js
+++ Y/api.js
@@ -1,32 +1,39 @@
-$(document).ready(function() {
-	console.log("api.js laoded!");
+// $(document).ready(function() {
+// 	console.log("api.js laoded!");
 
-	var secondaryAppConfig = {
-    authDomain: "what-the-blush.firebaseapp.com",
-    databaseURL: "https://makeup-api.herokuapp.com/api/v1/products.json/",
-    storageBucket: "what-the-blush.appspot.com",
-    };
-    
-    // var secondary = firebase.initializeApp(secondaryAppConfig, "secondary");
-    // var secondaryDatabase = secondary.database();
+// 	var secondaryAppConfig = {
+//     authDomain: "what-the-blush.firebaseapp.com",
+//     databaseURL: "https://makeup-api.herokuapp.com/api/v1/products.json/",
+//     storageBucket: "what-the-blush.appspot.com",
+//     };
 
-     // Use the otherApp variable to retrieve the other app's services
-    // var otherStorage = secondary.storage();
-    // var otherDatabase = secondary.database();
+// 	document.getElementById("hit").addEventListener('click', myFunction, false);
+// });
+// var result;
+// function myFunction() {
+//     var brands = document.getElementById("makeupName").innerHTML;
+//     console.log(brands);
+//     var url = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=' + brands + '';
+//     $.ajax ({
+//         method: 'GET',
+//         url: url,
+//         dataType: "json",
+//         success: function(data) {
+//             console.log('hello!'+ JSON.stringify(data[0].brand));
+//             result = JSON.stringify(data[0].brand);
+//             saveToList();
+//         }
+//     });
+//  }   
 
-	document.getElementById("makeupName").addEventListener('keypress', myFunction, false);
-});
-
-function myFunction() {
-    var brand = document.getElementById("makeupName").innerHTML;
-    var url = 'https://makeup-api.herokuapp.com/api/v1/products.json?brand=' + brand;
-    $.ajax ({
-        method: 'GET',
-        url: url,
-        dataType: "json",
-        success: function(res) {
-            console.log('hello');
-            console.log(res); 
-        }
-    });
- }   
+//  function saveToList(event) {
+//     //when user hits enter, will attempt to save data
+//     if(event.which == 13 || event.keyCode == 13) {
+//         var makeupName = document.getElementById('makeupName').value.trim();
+//         if(makeupName.length > 0) {
+//             saveToFB(makeupName);
+//         }
+//         document.getElementById('makeupName').value = '';
+//         return false;
+//     }
+// }
--- Y/app.js
+++ Y/app.js
@@ -1,32 +1,61 @@
 //client front side
 var wishMakeup = new Firebase('https://what-the-blush.firebaseio.com/');
 
-function saveToList(event) {
-	//when user hits enter, will attempt to save data
-	if(event.which == 13 || event.keyCode == 13) {
-		var makeupName = document.getElementById('makeupName').value.trim();
-		if(makeupName.length > 0) {
-			saveToFB(makeupName);
-		}
-		document.getElementById('makeupName').value = '';
-		return false;
-	}
+$(document).ready(function() {
+	console.log("api.js laoded!");
+
+	var secondaryAppConfig = {
+    authDomain: "what-the-blush.firebaseapp.com",
+    databaseURL: "https://makeup-api.herokuapp.com/api/v1/products.json/",
+    storageBucket: "what-the-blush.appspot.com",
+    };
+});
+document.getElementById("hit").addEventListener('click', myFunction, false);
+
+var result;
+function myFunction() {
+    var brands = document.getElementById("makeupName").innerHTML;
+    console.log(brands);
+    var url = 'http://makeup-api.herokuapp.com/api/v1/products.json?brand=' + brands + '';
+    $.ajax ({
+        method: 'GET',
+        url: url,
+        dataType: "json",
+        success: function(data) {
+            console.log('hello!'+ JSON.stringify(data[0].brand));
+            result = JSON.stringify(data[0].brand);
+            saveToList();
+        }
+    });
+ }   
+
+ function saveToList(event) {
+    //when user hits enter, will attempt to save data
+    
+        var makeupName = document.getElementById('makeupName').value.trim();
+        if(makeupName.length > 0) {
+            saveToFB(makeupName);
+        }
+        document.getElementById('makeupName').value = '';
+        return false;
 }
 
 //save data to Firebase
 function saveToFB(makeupName) {
 	wishMakeup.push({
-		name: makeupName
+		name: makeupName,
+		returnedBrand: result
 	});
 }
 
 function refreshUI(list) {
 	var lis = '';
 	for (var i = 0; i < list.length; i++) {
-		lis += '<li data-key="' + list[i].key + '">' + list[i].name + ' [' + genLinks(list[i].key, list[i].name) + ']</li>';
+		lis += '<li data-key="' + list[i].key + '">' + list[i].name + list[i].returnedBrand + ' [' + genLinks(list[i].key, list[i].name, list[i].returnedBrand) + ']</li>';
 	}
 	document.getElementById('wishMakeup').innerHTML = lis;
 }
+
 
 function genLinks(key, muName) {
 	var links = '';
