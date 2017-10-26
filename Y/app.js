426ec550575a2e80ed948b180529478b532fca50
roxnlopez <roxnlopez@gmail.com>
2017-10-25 22:10:37 -0600

    added modal and api to modal

--- Y/app.js
+++ Y/app.js
@@ -8,26 +8,67 @@ $(document).ready(function() {
     authDomain: "what-the-blush.firebaseapp.com",
     databaseURL: "https://makeup-api.herokuapp.com/api/v1/products.json/",
     storageBucket: "what-the-blush.appspot.com",
-    };
+   };
+
+   $('#productTypeForm').on('submit', function (e) { e.preventDefault(); retrieveBrands(); });
 });
-document.getElementById("hit").addEventListener('click', myFunction, false);
+
+function filterBrands(brands) {
+
+  // Filter out nulls
+	brands = brands.filter(function (brand) { return brand; });
+
+	// Get unique brands
+	brandSet = {}; // Keys of an object form a set
+	brands.map(function (brand) { brandSet[brand] = 1; });
+	brands = Object.keys(brandSet);
+
+	return brands;
+}
 
 //var result; // DONT USE GLOBAL VARIABLES
 function retrieveBrands() { //myFunction() { // BETTER FUNCTION NAMES
     var productType = document.getElementById("productType").value.trim();
-    console.log(brands);
     var url = 'https://makeup-api.herokuapp.com/api/v1/products.json?product_type=' + productType;
     $.ajax ({
         method: 'GET',
         url: url,
         dataType: "json",
         success: function(data) {
-            console.log('hello!'+ JSON.stringify(data[1].brand));
-            var brand = JSON.stringify(data[1].brand);
-            saveToList(productType, brand);
+        	var brands = data.map(function (datum) { return datum.brand; });
+
+        	brands = filterBrands(brands)
+
+            // saveToList(productType, brand); 
+            displayProductListModal(productType, brands);
+            $('#productListModal').modal('show');
         }
     });
  }
+
+ function displayProductListModal(productType, brands) {
+ 	console.log('displayProductListModal', productType, brands);
+
+ 	$('#productTypeLabel').text(productType);
+
+	var content = '';
+	brands.map(function (brand) {
+		content += '<a class="item">' + brand + '</a>';
+	});
+ 	$('#productList').children().remove();
+ 	$('#productList').append(content);
+
+ 	$('#productList .item').on('click', function(e) {
+ 		e.preventDefault();
+ 		var brand = $(e.target).text();
+ 		saveToList(productType, brand);
+ 		$('#productListModal').modal('hide');
+ 	});
+
+ 	$('#productListModal').modal('show');
+
+ }
+
 //synthetic or cached database firebase
 function saveToList(productType, brand) { // Pass brand into saveToList, don't use global vars
     //when user hits enter, will attempt to save data
@@ -48,15 +89,58 @@ function saveToFB(productType, brand) {
 	});
 }
 
+/*
+<div class="item">
+		<i class="icon paint brush"></i>
+	<div class="content">
+		<span class="brandName">Aveda</span> <span class="productType">mascara</span>
+		<div class="ui mini buttons">
+			<button class="ui button">Edit</button>
+			<button class="ui button">Delete</button>
+			<button class="ui button"><i class="heart icon"></i></button>
+		</div>
+	</div>
+</div>
+*/
+
 function refreshUI(list) {
 	var lis = '';
   console.log('refreshUI', list);
 	for (var i = 0; i < list.length; i++) {
 		// console.log(result);
-		lis += '<li data-key="' + list[i].key + '">' + list[i].name + ' ' + list[i].returnedBrand + ' [' + genLinks(list[i].key, list[i].name, list[i].returnedBrand) + ']</li>';
+		//lis += '<li data-key="' + list[i].key + '">' + list[i].name + ' ' + list[i].returnedBrand + ' [' + genLinks(list[i].key, list[i].name, list[i].returnedBrand) + ']</li>';
+		var key = list[i].key;
+		var brand = list[i].returnedBrand;
+		var productType = list[i].name;
+		lis +=  '<div class="item">' +
+		         	'<i class="icon paint brush"></i>' +
+	            	'<div class="content">' +
+		            	'<span class="brandName">' + brand + '</span> <span class="productType">' + productType + '</span>' +
+								  	'<div class="ui mini buttons">' +
+											'<button class="ui edit button" data-key="'+key+'" data-product="'+productType+'">Edit</button>' +
+											'<button class="ui delete button" data-key="'+key+'" data-product="'+productType+'">Delete</button>' +
+											'<button class="ui heart button"><i class="heart icon"></i></button>' +
+										'</div>' +
+									'</div>' +
+								'</div>';
 		// console.log(result);
 	}
 	document.getElementById('wishMakeup').innerHTML = lis;
+
+	$('#wishMakeup .button').on('click', function(e) {
+		e.preventDefault();
+		var btn = $(e.target);
+		var key = btn.data('key');
+		var productType = btn.data('product');
+
+		if (btn.hasClass('edit')) {
+			edit(key, productType);
+		} else if (btn.hasClass('delete')) {
+			del(key, productType);
+		} else if (btn.hasClass('heart')) {
+			// TODO
+		}
+	});
 }
 
 
