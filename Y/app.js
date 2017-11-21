b296bd67d19a8cabac7d1cb0d7fa9a1bd8a62979
roxnlopez <roxnlopez@gmail.com>
2017-11-20 19:22:21 -0700

    tried to add gmail but no go right now

--- Y/app.js
+++ Y/app.js
@@ -141,23 +141,6 @@ function genLinks(key, muName) {
 
 //modal
 console.log("Tuesday");
-// var modal = document.getElementById('myModal');
-// var btn = document.getElementById('hit');
-// var span = document.getElementsByClassName("close")[0];
-
-// btn.onclick = function() {
-// 	modal.style.display = "block";
-// };
-
-// span.onclick = function() {
-// 	modal.style.display = "none";
-// };
-// console.log("Wednesday");
-// window.onclick = function(event) {
-// 	if(event.target == modal) {
-// 		modal.style.display = "none";
-// 	}
-// };
 
 //edit
 function edit(key, muName) {
